04d0df8d625ce99024690decc038dbc609714b45
Hmddev23 <hamid.maira709@gmail.com>
2023-10-19 16:04:48 -0700

    Starting

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  * main - Entry point of the Monty interpreter program.
+  *
+  * @argc: Command-line argument counter.
+  * @argv: Command-line argument values.
+  *
+  * Return: 0 on success.
+  */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
